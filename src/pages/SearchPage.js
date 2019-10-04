3a078c337918dbfd4e161dde5987fa1ceb139adf
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-04 09:01:45 +0200

    chore: Add lodash

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import _ from "lodash";
+
+import { search } from "./../BooksAPI";
+
+import Book from "./components/Book";
 
 class SearchPage extends Component {
-  state = {};
+  state = {
+    query: "",
+    results: []
+  };
+
+  handleChange = _.debounce(async query => {
+    console.log(query);
+    this.setState({ query });
+    const results = await search(query);
+    this.setState({ results });
+  }, 500);
+
   render() {
+    const { query, results } = this.state;
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -19,12 +36,28 @@ class SearchPage extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={e => {
+                this.handleChange(e.target.value);
+              }}
+            />
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+
+        {results.length > 0 && (
+          <div className="search-books-results">
+            <p>
+              Showing {results.length} for search {query}.
+            </p>
+            <ol className="books-grid">
+              {results.map(result => (
+                <Book book={result} />
+              ))}
+            </ol>
+          </div>
+        )}
       </div>
     );
   }
